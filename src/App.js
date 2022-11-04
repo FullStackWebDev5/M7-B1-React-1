7b580a5580400e8f07cb2f5960a2881dc24df8ca
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-11-04 23:36:58 +0530

    React - Components, overview, JSX, export default, Componest nesting, reusing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import "./App.css";
+import Component1 from "./Component1";
+import Component2 from "./Component2";
 
 function App() {
   return (
     <div className="App">
-      We are learning React!
+      <h1 className="page-title">My First React App</h1>
+      <Component1 />
+			<Component2 />
     </div>
   );
 }
 
 export default App;
+
+/*
+	REACT
+	- SPA
+	- Uses Virtual DOM 
+
+	Declarative Library
+  - Tell react WHAT to do
+  - Don't tell react HOW to do, react handles it
+
+	JSX: JavaScript + XML
+
+	Components - (which returns JSX)
+	Two types:
+	- Class Based Components (JavaScript Classes)
+	- Function Based components (JavaScript Functions)
+	Properties:
+  - Create parts of UI
+  - Can be nested
+  - Can be re-used
+*/
